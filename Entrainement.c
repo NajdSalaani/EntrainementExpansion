1eeedae2c7ddea67116a8225dd8d49a219efa2eb
NajdSalaani <dimnajd@outlook.fr>
2023-02-15 13:47:37 +0100

    initialisation des bords

--- Entrainement.c
+++ Entrainement.c
@@ -35,15 +35,36 @@ int Next; // numéro du prochain trésor du joueur
 	Cette fonction créer et initialise un labyrinthe 4*4 
 	Elle renvoie l'adresse de la première tuile du labyrinthe 
 */
-	void RemplissageLab(t_tile * PremiereTuile){
+	void InitialisationLab(t_tile * PremiereTuile){
 	
 		PremiereTuile = malloc(16*sizeof(t_tile));
-	
+		
+		// initialisation des tiles items D'abord tout à zéro puis placement trésor
+		for(int i = 0; i<16 ; i++){
+			PremiereTuile[i].tileItem = 0;
+		}
+		PremiereTuile[11].tileItem = 3;
+		
+		//initialisation des bords du plateaux
+		for(int i = 0; i<4 ; i++){
+			PremiereTuile[i].tileN = 1; //Haut
+		}
+		//initialisation des bords du plateaux
+		for(int i = 13; i<16 ; i++){
+			PremiereTuile[i].tileS = 1; //Bas
+		}
+		for(int i = 3; i<16 ; i+4){
+			PremiereTuile[i].tileW = 1; //Droit
+		}
+		for(int i = 0; i<14 ; i+4){
+			PremiereTuile[i].tileE = 1; //Gauche
+		}
 	}
 	
 	
 //Main 
 
 int main(){
+	printf("ok \n");
 	return 1;
 }
