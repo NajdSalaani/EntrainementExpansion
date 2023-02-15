d0ec70dca99020192950579784ec0ee9e6b2d681
NajdSalaani <dimnajd@outlook.fr>
2023-02-16 00:37:26 +0100

    Nouvel initialisation tab labyrinthe ok avec test bon

--- Entrainement.c
+++ Entrainement.c
@@ -25,72 +25,79 @@ int tileItem; // numero item ou 0 si aucun
 //Pour le labyrinthe 
 
 typedef struct {
-t_tile* p_lab; //Pointeur vers la première tuile du labyrinthe
+t_tile p_lab[16]; //tableau de tuiles
 int playerX,playerY; //coordonnées du joueur (nous/notre bot)
 int Next; // numéro du prochain trésor du joueur
 } t_lab;
 
 
-/* Fonction de création du Labyrinthe 
-	Cette fonction créer et initialise un labyrinthe 4*4 
-	Elle renvoie l'adresse de la première tuile du labyrinthe 
-*/
-	void InitialisationLab(t_tile * PremiereTuile){
-	
-		PremiereTuile = malloc(16*sizeof(t_tile));
+
 		
-		// initialisation des tiles items D'abord tout à zéro puis placement trésor
+		
+	
+
+int main(){
+
+//Creation et initialisation du labyrinthe ( ce peut être une copie du labyrinthe pour ne pas le perdre lors de l'expansion
+t_lab Labyrinthe;
+// initialisation des tiles D'abord tout à zéro puis placement trésor etc
 		for(int i = 0; i<16 ; i++){
-			PremiereTuile[i].tileItem = 0;
+			Labyrinthe.p_lab[i].tileItem = 0;
+			Labyrinthe.p_lab[i].tileN = 0;
+			Labyrinthe.p_lab[i].tileS = 0;
+			Labyrinthe.p_lab[i].tileW = 0;
+			Labyrinthe.p_lab[i].tileE = 0;
 		}
-		PremiereTuile[11].tileItem = 3;
+		Labyrinthe.p_lab[11].tileItem = 3;
 		
 		//initialisation des bords du plateaux
 		for(int i = 0; i<4 ; i++){
-			PremiereTuile[i].tileN = 1; //Haut
+			Labyrinthe.p_lab[i].tileN = 1; //Haut
 		}
-		for(int i = 13; i<16 ; i++){
-			PremiereTuile[i].tileS = 1; //Bas
+		for(int i = 12; i<16 ; i++){
+			Labyrinthe.p_lab[i].tileS = 1; //Bas
 		}
-		for(int i = 3; i<16 ; i+4){
-			PremiereTuile[i].tileE = 1; //Droit
+		for(int i = 3; i<16 ; i++){
+			Labyrinthe.p_lab[i].tileE = 1; //Droit
+			i = i+3;
 		}
-		for(int i = 0; i<14 ; i+4){
-			PremiereTuile[i].tileW = 1; //Gauche
+		for(int i = 0; i<14 ; i++){
+			Labyrinthe.p_lab[i].tileW = 1; //Gauche
+			i = i+3;
 		}
 		
 		//initialisation du reste
 		//Bas
-		PremiereTuile[0].tileS = 1;
-		PremiereTuile[1].tileS = 1;
-		PremiereTuile[4].tileS = 1;
-		PremiereTuile[7].tileS = 1;
-		PremiereTuile[9].tileS = 1;
-		PremiereTuile[10].tileS = 1;
+		Labyrinthe.p_lab[0].tileS = 1;
+		Labyrinthe.p_lab[1].tileS = 1;
+		Labyrinthe.p_lab[4].tileS = 1;
+		Labyrinthe.p_lab[7].tileS = 1;
+		Labyrinthe.p_lab[9].tileS = 1;
+		Labyrinthe.p_lab[10].tileS = 1;
 		//Haut
-		PremiereTuile[4].tileN = 1;
-		PremiereTuile[8].tileN = 1;
-		PremiereTuile[9].tileN = 1;
-		PremiereTuile[11].tileN = 1;
-		PremiereTuile[12].tileN = 1;
-		PremiereTuile[15].tileN = 1;
+		Labyrinthe.p_lab[4].tileN = 1;
+		Labyrinthe.p_lab[8].tileN = 1;
+		Labyrinthe.p_lab[9].tileN = 1;
+		Labyrinthe.p_lab[11].tileN = 1;
+		Labyrinthe.p_lab[12].tileN = 1;
+		Labyrinthe.p_lab[15].tileN = 1;
 		//Droite
-		PremiereTuile[2].tileE = 1;
-		PremiereTuile[5].tileE = 1;
-		PremiereTuile[6].tileE = 1;
-		PremiereTuile[9].tileE = 1;
+		Labyrinthe.p_lab[2].tileE = 1;
+		Labyrinthe.p_lab[5].tileE = 1;
+		Labyrinthe.p_lab[6].tileE = 1;
+		Labyrinthe.p_lab[9].tileE = 1;
 		//Gauche
-		PremiereTuile[3].tileW = 1;
-		PremiereTuile[5].tileW = 1;
-		PremiereTuile[6].tileW = 1;
-		PremiereTuile[9].tileW = 1;
-		PremiereTuile[13].tileW = 1;
-	}
+		Labyrinthe.p_lab[3].tileW = 1;
+		Labyrinthe.p_lab[5].tileW = 1;
+		Labyrinthe.p_lab[6].tileW = 1;
+		Labyrinthe.p_lab[9].tileW = 1;
+		Labyrinthe.p_lab[13].tileW = 1;
 	
 	
-//Main 
 
-int main(){
+
+
+	//Fin
 	printf("ok \n");
 	return 1;
 }
